46e409d443fb66c4d6df881a81f03e71a662062a
Bilel Benamira <bbenamira@qovery.com>
2022-01-31 14:53:11 +0100

    add log

---
+++ go.mod
@@ -0,0 +1,10 @@
+module go-http-server
+
+go 1.17
+
+require github.com/sirupsen/logrus v1.8.0
+
+require (
+	github.com/magefile/mage v1.10.0 // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+)
--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	log "github.com/sirupsen/logrus"
 )
 
 const PORT = 8080
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Hello Workd\n")
+		log.WithField("path", "/").Infof("Incomming Request")
+		if _, err := fmt.Fprintf(w, "Hello Workd\n"); err != nil {
+			log.WithError(err).Fatal("unexpected error")
+		}
 	})
-	fmt.Printf("Serving on port :%d\n", PORT)
-	http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil)
+
+	log.WithField("port", PORT).Info("Serving http server...")
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil); err != nil {
+		log.WithError(err).Fatal("unexpected error")
+		return
+	}
 }
