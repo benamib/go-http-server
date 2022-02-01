fd6ba131a61030bcf6ade040fca60a4e820ea447
Bilel Benamira <bbenamira@qovery.com>
2022-02-01 10:35:51 +0100

    feat: add healthcheck

--- go.mod
+++ go.mod
@@ -4,10 +4,13 @@ go 1.17
 
 require (
 	github.com/go-chi/chi/v5 v5.0.2
+	github.com/hellofresh/health-go/v4 v4.4.0
 	github.com/sirupsen/logrus v1.8.0
 )
 
 require (
 	github.com/magefile/mage v1.10.0 // indirect
-	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+	go.opentelemetry.io/otel v1.0.0 // indirect
+	go.opentelemetry.io/otel/trace v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
 )
--- main.go
+++ main.go
@@ -6,14 +6,21 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/hellofresh/health-go/v4"
 	log "github.com/sirupsen/logrus"
 )
 
 const PORT = 8080
 
 func main() {
+	healthCheck, err := health.New()
+	if err != nil {
+		log.WithError(err).Fatal("failed to init health check")
+	}
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Get("/status", healthCheck.HandlerFunc)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		if _, err := fmt.Fprintf(w, "Hello World\n"); err != nil {
 			log.WithError(err).Fatal("unexpected error")
